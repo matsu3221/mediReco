05243c76f4e73c4a7a25fa65efa3df0cdcd10550
matsumoto tadataka <yu0i0so0n@gmail.com>
2021-02-23 23:11:41 +0900

    サイドメニューの追加&Drag&Dropの追加

--- mediReco/src/app/app-routing.module.ts
+++ mediReco/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/' },
+  { path: 'bookList', component: BookListComponent }, 
+  { path: 'home', component: HomeComponent }, 
+  { path:'root', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mediReco/src/app/app.module.ts
+++ mediReco/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+
+import { MatIconModule } from '@angular/material/icon';  // <-- 追加
+import { MatToolbarModule } from '@angular/material/toolbar';  // <-- 追加
+import { MatSidenavModule } from '@angular/material/sidenav';  // <-- 追加
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    AppRoutingModule,
+    DragDropModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
